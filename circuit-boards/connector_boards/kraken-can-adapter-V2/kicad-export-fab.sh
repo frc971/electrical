d2740ef4efa99682aeec9bc78d0eb150ed68afe5
Scott Berman <scott.berman@bluerivertech.com>
2024-06-22 15:47:35 -0700

    Fixed kraken-can-adapter V2.
    Signed-off-by: Scott Berman <scott.berman@bluerivertech.com>

--- circuit-boards/connector_boards/kraken-can-adapter-V2/kicad-export-fab.sh
+++ circuit-boards/connector_boards/kraken-can-adapter-V2/kicad-export-fab.sh
@@ -4,17 +4,20 @@ set -x
 #fname=NX-J401-Adapter
 fname=$1
 
+kicad_local_plugins="$HOME/.local/share/kicad/8.0/3rdparty/plugins/"
+ibom_exe="org_openscopeproject_InteractiveHtmlBom/generate_interactive_bom.py"
 sch=${fname}.kicad_sch
 pcb=${fname}.kicad_pcb
-pcb_fab_layers=User.1,User.Drawings,User.Comments,Edge.Cuts
+pcb_fab_layers=User.1,Edge.Cuts
 #F.Mask
+#2 Layer PCB
 gerber_layers=F.Cu,B.Cu,F.Paste,B.Paste,F.Mask,B.Mask,Edge.Cuts,F.Silkscreen,B.Silkscreen
+p#4 layer PCB
+#gerber_layers=F.Cu,In1.Cu,In2.Cu,B.Cu,F.Paste,B.Paste,F.Mask,B.Mask,Edge.Cuts,F.Silkscreen,B.Silkscreen
 pcb_assy_top_layers=Edge.Cuts,F.Fab
 pcb_assy_bot_layers=Edge.Cuts,B.Fab
 pcb_assembly_layers=User.2
 src_files="./Analysis/*","./Components/*","*.kicad_sch","*.kicad_dru","*.kicad_pcb","*.kicad_prl","*.kicad_pro","fp-info-cache","fp_lib-table","3dModels/*","*.pretty"
-
-
 
 function export_gerber () {
     layer=$1
@@ -40,7 +43,7 @@ mkdir -p ${GERBER_DIR}
 #generate schematic pdf
 kicad-cli sch export pdf --output "${SCH_DIR}/${fname}-sch-$timestamp.pdf" ${sch}
 
-#generate PCB gerbers
+generate PCB gerbers
 for layer in ${gerber_layers//,/ }
 do
     export_gerber ${layer}
@@ -53,28 +56,21 @@ kicad-cli pcb export drill --output ${GERBER_DIR}/ ${pcb}
 #kicad-cli pcb export drill --output ${TEMP_DIR}/ --generate-map --map-format svg ${pcb}
 
 #generate PCB Fab Drawing
-kicad-cli pcb export pdf --output ${GERBER_DIR}/${fname}-fab-${timestamp}.pdf --layers ${pcb_fab_layers} --include-border-title --black-and-white ${pcb}
+kicad-cli pcb export pdf --output ${GERBER_DIR}/${fname}-fab-${timestamp}.pdf --layers ${pcb_fab_layers} --include-border-title --black-and-white ${pcb} 
 
 #generate BOM from schematic
-#kicad-cli sch export netlist --output "${SCH_DIR}/${fname}-netlist.xml" ${sch}
-#python3 ${plugin_path}/bom_csv_grouped_by_value.py ${SCH_DIR}/${fname}-netlist.xml "${SCH_DIR}/${fname}-bom.csv"
+kicad-cli sch export bom --sort-field "Reference" --sort-asc --group-by "MFG P/N" --fields "\${Item},\${QUANTITY},Reference,Value,MFG,MFG P/N,DIST,DIST P/N" --ref-range-delimiter "-" --exclude-dnp --output ${FAB_DIR}/${fname}-bom.csv $fname.kicad_sch 
 
-#cp ${fname}.csv ${FAB_DIR}/${fname}-bom.csv
-cp ./bom/ibom.html ${FAB_DIR}/${fname}-ibom-${timestamp}.html
-./kicad-group-bom.py -i ${fname}.csv -o ${FAB_DIR}/${fname}-bom-${timestamp}.csv
+#generate Assembly Drawing
+kicad-cli pcb export pdf --output ${FAB_DIR}/${fname}-assy-${timestamp}.pdf --layers ${pcb_assembly_layers} --include-border-title --black-and-white ${pcb} 
 
-#generate PCB assembly drawing
-#kicad-cli pcb export pdf --output ${TEMP_DIR}/${fname}-top-assy.pdf --layers ${pcb_assy_top_layers} --exclude-value --black-and-white ${pcb}
-#kicad-cli pcb export pdf --output ${TEMP_DIR}/${fname}-bot-assy.pdf --layers ${pcb_assy_bot_layers} --exclude-value --black-and-white ${pcb}
+#generate Interactive BOM
 
-#pdf2svg ${TEMP_DIR}/${fname}-top-assy.pdf ${TEMP_DIR}/${fname}-top-assy.svg
-#pdf2svg ${TEMP_DIR}/${fname}-bot-assy.pdf ${TEMP_DIR}/${fname}-bot-assy.svg
-
-kicad-cli pcb export pdf --output ${FAB_DIR}/${fname}-assy-${timestamp}.pdf --layers ${pcb_assembly_layers} --include-border-title --black-and-white ${pcb}
+python3 $kicad_local_plugins/$ibom_exe --show-dialog $pcb
 
 #generate PnP file
-kicad-cli pcb export pos --output ${FAB_DIR}/${fname}-top-pos.txt --side front ${pcb}
-kicad-cli pcb export pos --output ${FAB_DIR}/${fname}-bot-pos.txt --side back ${pcb}
+kicad-cli pcb export pos --output ${FAB_DIR}/${fname}-top-pos.txt --side front ${pcb} 
+kicad-cli pcb export pos --output ${FAB_DIR}/${fname}-bot-pos.txt --side back ${pcb} 
 
 #create zip file
 #zip -r ${SCH_DIR}/${fname}-src-${timestamp}.zip ${src_files//,/ } 
@@ -85,6 +81,7 @@ pushd ${SCH_DIR}
 zip ${fname}-step-${timestamp}.zip ${fname}.step
 rm ${fname}.step
 popd
+
 
 #create zip packages
 pushd ${ZIP_DIR}
